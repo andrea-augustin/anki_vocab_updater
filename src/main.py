449cfba43921de791c329d17156510c410a3782c
Andrea Augustin <augustin.andrea@campus.lmu.de>
2021-04-13 11:31:18 +0200

    implemented following: load words from text file, get meanings of a word, get translation of a word using mymemory; WIP: adding a new note to an anki deck (works with dummy data so far)

---
+++ src/main.py
@@ -0,0 +1,79 @@
+import requests
+
+from ankipandas import Collection
+from tkinter import filedialog
+from nltk.corpus import wordnet
+
+
+def get_words_from_text_file():
+    filepath = filedialog.askopenfilename()
+    with open(filepath, encoding='utf-8') as f:
+        file_content = f.read()
+
+    words = file_content.split('\n')
+
+    return words
+
+
+def get_meanings_of_a_word(word):
+    meanings = []
+    synsets_of_word = wordnet.synsets(word)
+
+    for synset in synsets_of_word:
+        meanings.append([synset, synset.definition()])
+
+    return meanings
+
+
+def get_translation_of_a_word(word, language_code):
+    url = "https://api.mymemory.translated.net/get?q=" + word + "&langpair=" + language_code + "|de"
+    response = requests.post(url).json()
+
+    return response['responseData']['translatedText']
+
+
+def collect_information_for_words(list_of_words):
+    output = []
+
+    """
+    create output list
+    for each word:
+        get meaning of word
+        get translation of word
+        get pronunciation of word
+        put all information into a list [word_en, word_de, word_pronunciation, word_meaning]
+        add new list element to output list
+    return output list
+    """
+
+
+def anki_stuff():
+    """
+    open collection
+    get all cards from target deck
+    for each new word - check if note is already list of cards from above
+        yes: skip
+        no: collect
+    add all new notes via add_notes(nmodel, [[word1_en, word1_de, word1_word_pronunciation, word1_meaning], [word2_en, ...]]
+    save changes via col.write(add=True)
+    """
+
+    # always needed
+    col = Collection()
+
+    # adds one new note (function returns an object!) and saves it into the collection
+    test = col.notes.add_note('Englisch', ['englisches wort', 'deutsches wort', 'aussprache', 'bedeutung'], inplace=True)
+    col.write(add=True)
+
+    # lists all decks with cards in them
+    print(col.cards.list_decks())
+
+
+
+def main():
+    collect_information_for_words(['dogma'])
+    print(1231)
+
+
+if __name__ == '__main__':
+    main()
